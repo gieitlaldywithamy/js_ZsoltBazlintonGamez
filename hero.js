2d0e5f51fc3d53edabd987b7519b097b6e281d28
Amy <amy.j.morrison@icloud.com>
2018-02-17 19:23:38 +0000

    hero can eat gets twice health restore if favourite food

--- hero.js
+++ hero.js
@@ -8,4 +8,16 @@ const Hero = function(name, health, favouriteFood){
 Hero.prototype.talk = function(){
   return `I am ${this.name}`;
 }
+
+Hero.prototype.addTask = function(newTask){
+  this.tasks.push(newTask);
+}
+
+Hero.prototype.eat = function(food){
+  if (this.favouriteFood == food){
+    this.health += food.replenishmentValue * 1.5;
+  } else {
+    this.health += food.replenishmentValue;
+  }
+}
 module.exports = Hero;
--- specs/testhero.js
+++ specs/testhero.js
@@ -7,15 +7,20 @@ describe('Hero', function() {
   let hero;
   let tablet;
   let omelette;
+  let task1;
+  let task2;
+  let task3;
 
   beforeEach(function(){
     tablet = new Food('tablet', 6);
     omelette = new Food('omelette', 15);
-    hero = new Hero('Ethel McDonald', 40, tablet);
+    hero = new Hero('william wallace', 40, tablet);
+    task1 = new Task('raid Scone', 5, 2, 'take a king as prisoner')
+    task2 = new Task('collect arrows', 3, 4, 'has more weapons');
   });
 
   it('has a name', function(){
-    assert.strictEqual(hero.name, "Ethel McDonald");
+    assert.strictEqual(hero.name, "william wallace");
   });
 
   it('has health', function(){
@@ -27,11 +32,27 @@ describe('Hero', function() {
   });
 
   it('can talk', function(){
-    assert.strictEqual(hero.talk(), 'I am Ethel McDonald');
+    assert.strictEqual(hero.talk(), 'I am william wallace');
   });
 
   it('has a collection of tasks which starts empty', function(){
     assert.strictEqual(hero.tasks.length, 0);
+  });
+
+  it('can add tasks', function(){
+      hero.addTask(task1);
+      hero.addTask(task2);
+      assert.deepEqual(hero.tasks, [task1, task2]);
+  });
+
+  it('can eat food and restore replenishmentValue', function(){
+    hero.eat(omelette);
+    assert.strictEqual(hero.health, 55);
+  });
+
+  it('can eat favourite food and restore by replenishmentValue * 1.5', function(){
+    hero.eat(tablet);
+    assert.strictEqual(hero.health, 49);
   });
 
 //   A hero should be able to eat food, and health should go up by the replenishment value
